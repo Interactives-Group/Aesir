040b68df4eb1cebd0bc0a7acddd64bfa779c0b39
Lucas Puntillo <62703475+puntillol59@users.noreply.github.com>
2020-11-23 09:29:54 -0500

    Create build-linux.sh

---
+++ build-linux.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/puntillol59/Aesir.git
+cd Aesir 
+npm init
+npm install gulp --save-dev
+npm install gulp-util --save-dev
+npm install gulp-tap --save-dev
+npm install markdown-it --save-dev
+npm install markdown-it-alerts --save-dev
+npm install markdown-it-fontawesome --save-dev
