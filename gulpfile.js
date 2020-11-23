232849fbbf5274e0f17473cf6cf2ee4b0ee89fa3
Lucas Puntillo <62703475+puntillol59@users.noreply.github.com>
2020-11-23 15:00:37 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ md.renderer.rules.table_close = function (tokens, idx, options, env, self) {
         + '</div>\n';
 };
 
-module.exports.default = watch('*', clean)
+module.exports.default = return gulp.src('articles/**/*.md')
 
 gulp.task('build', function() {
     return gulp.src('articles/**/*.md')
