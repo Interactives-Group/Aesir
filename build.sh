4328a32da6be826330be67a8267483bcfb8cd7cd
Lucas Puntillo <62703475+puntillol59@users.noreply.github.com>
2020-11-25 09:29:13 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 echo "NOTICE: You need node.js and npm installed!"
 git clone https://github.com/puntillol59/Aesir.git
 cd Aesir 
+npm install --global gulp-cli
 npm install gulp --save-dev
 npm install gulp-util --save-dev
 npm install gulp-tap --save-dev
@@ -8,4 +9,4 @@ npm install markdown-it --save-dev
 npm install markdown-it-alerts --save-dev
 npm install markdown-it-fontawesome --save-dev
 echo "Success!"
-gulp
+gulp && cd articles && ls
